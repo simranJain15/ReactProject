85e4e592a64161d826d243b7c6bff3152e4672b8
simranJain15 <165780190+simranJain15@users.noreply.github.com>
2024-04-05 16:30:40 +0530

    Delete src directory

--- src/App.js
+++
@@ -1,8 +0,0 @@
-
-function App() {
-  return (
-    <h1>SSP Industrial mart</h1>
-  );
-}
-
-export default App;
--- src/SP.js
+++
@@ -1,7 +0,0 @@
-function Sp() {
-    return(
-        <h1>SP is best--</h1>
-    )
-}
-
-export default Sp;
