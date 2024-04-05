84f2c9e2164bdc008c6cdd0f652f9d24b143596a
simran Jain <simranjain151996@gmail.com>
2024-04-05 16:04:53 +0530

    basic react project

---
+++ src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>SSP Industrial mart</h1>
+  );
+}
+
+export default App;
---
+++ src/SP.js
@@ -0,0 +1,7 @@
+function Sp() {
+    return(
+        <h1>SP is best--</h1>
+    )
+}
+
+export default Sp;
